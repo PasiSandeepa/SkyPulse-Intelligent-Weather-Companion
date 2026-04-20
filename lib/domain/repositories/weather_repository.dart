a9b3a4a1156f7de2cf7e9b93556ff7a28e4a2c11
PasiSandeepa <pasisandeepa456@gmail.com>
2026-04-20 14:08:46 +0530

    feat(repository): define weather repository abstraction layer

---
+++ lib/domain/repositories/weather_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import '../entities/weather_entity.dart';
+
+abstract class WeatherRepository {
+  Future<Either<String, WeatherEntity>> getWeatherByCity(String cityName);
+  Future<Either<String, WeatherEntity>> getWeatherByLocation(double lat, double lon);
+  Future<Either<String, List<double>>> getForecast(double lat, double lon);
+}
