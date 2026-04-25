cf4f5373ff7aa8de32c2647fbde4b0633245138c
PasiSandeepa <pasisandeepa456@gmail.com>
2026-04-25 21:32:06 +0530

    feat(auth): create auth state with authentication status enum

---
+++ lib/presentation/bloc/auth/auth_state.dart
@@ -0,0 +1,32 @@
+part of 'auth_bloc.dart';
+
+enum AuthStatus { initial, loading, authenticated, unauthenticated, failure }
+
+class AuthState extends Equatable {
+  final AuthStatus status;
+  final UserEntity? user;
+  final String? errorMessage;
+
+  const AuthState({
+    this.status = AuthStatus.initial,
+    this.user,
+    this.errorMessage,
+  });
+
+  factory AuthState.initial() => const AuthState();
+
+  AuthState copyWith({
+    AuthStatus? status,
+    UserEntity? user,
+    String? errorMessage,
+  }) {
+    return AuthState(
+      status: status ?? this.status,
+      user: user ?? this.user,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, user, errorMessage];
+}
